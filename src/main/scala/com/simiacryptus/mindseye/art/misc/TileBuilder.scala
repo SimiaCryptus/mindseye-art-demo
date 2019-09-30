bea67ded7a1559aaa394073ea07aaee60a7b6200
Andrew Charneski <acharneski@gmail.com>
2019-09-29 22:23:01 -0700

    wip

--- src/main/scala/com/simiacryptus/mindseye/art/misc/TileBuilder.scala
+++ src/main/scala/com/simiacryptus/mindseye/art/misc/TileBuilder.scala
@@ -358,19 +358,19 @@ abstract class TileBuilder extends ArtSetup[Object] {
     selectRegion(img, positionX, positionY, width, height)
   }
 
-  def selectRegion(img: BufferedImage, positionX: Int, positionY: Int, width: Int, height: Int) = {
-    val tileSelectLayer = new ImgTileSelectLayer(width, height, positionX, positionY)
-    val result = tileSelectLayer.eval(Tensor.fromRGB(img)).getDataAndFree.getAndFree(0)
-    tileSelectLayer.freeRef()
-    result
-  }
-
   def selectTop(img: BufferedImage, size: Int) = {
     val positionX = 0
     val positionY = 0
     val width = img.getWidth
     val height = size
     selectRegion(img, positionX, positionY, width, height)
+  }
+
+  def selectRegion(img: BufferedImage, positionX: Int, positionY: Int, width: Int, height: Int) = {
+    val tileSelectLayer = new ImgTileSelectLayer(width, height, positionX, positionY)
+    val result = tileSelectLayer.eval(Tensor.fromRGB(img)).getDataAndFree.getAndFree(0)
+    tileSelectLayer.freeRef()
+    result
   }
 
   def selectLeft(img: BufferedImage, size: Int) = {
